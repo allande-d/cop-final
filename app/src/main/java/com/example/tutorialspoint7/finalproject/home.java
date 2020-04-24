a94946c8f9acb3e7d7c3a0e13f97888b1c843bda
allande-d <63927715+allande-d@users.noreply.github.com>
2020-04-24 16:22:26 -0400

    Added the movie title search and year search bars and added it to the functionality.

--- app/src/main/java/com/example/tutorialspoint7/finalproject/home.java
+++ app/src/main/java/com/example/tutorialspoint7/finalproject/home.java
@@ -2,7 +2,11 @@ package com.example.tutorialspoint7.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.tutorialspoint7.finalproject.ui.login.LogIn;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
@@ -21,6 +26,12 @@ import org.json.JSONObject;
 public class home extends AppCompatActivity {
     TextView mTextViewResult;
     ImageView imageView;
+    EditText movie_title;
+    EditText movie_year;
+    String hold_title;
+    String hold_year;
+    Button getMovie;
+    String movieid;
     private RequestQueue mQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +39,31 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         imageView = findViewById(R.id.imageView2);
         mTextViewResult = findViewById(R.id.textView5);
+        movie_title = findViewById(R.id.name);
+        movie_year = findViewById(R.id.editText);
+        getMovie = findViewById(R.id.button3);
         mQueue = Volley.newRequestQueue(this);
 
-        jsonParse();
+
+        getMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hold_title = movie_title.getText().toString();
+                hold_year = movie_year.getText().toString();
+                jsonParse();
+            }
+        });
 
     }
-    private void jsonParse() { String url = "https://www.omdbapi.com/?i=tt3896198&apikey=fe4f1194";
+    //http://www.omdbapi.com/?t=galaxy&apikey=fe4f1194
+    private void jsonParse() { String url = "https://www.omdbapi.com/?t=" + hold_title + "&y=" + hold_year + "&apikey=fe4f1194";
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         JSONArray weather = null;
-                        String hold = null;
-                        String city = null;
-                        String clouds = null;
+                        String movie_tit = null;
                         String poster = null;
 
                         try {
@@ -52,17 +73,16 @@ public class home extends AppCompatActivity {
                           //      hold = weather.getJSONObject(i).getString("Source");
                           // }
                             //clouds = response.getJSONObject("Ratings").getString("Source");
-                            city = response.getString("Title");
+                            movie_tit = response.getString("Title");
                             poster = response.getString("Poster");
+                            movieid = response.getString("imdbID");
                             //Picasso allows for hassle-free image loading in your application—often in one line of code!
                             Picasso.get().load(poster).into(imageView);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                       // mTextViewResult.setText("Weather in: " + city + "\n" + "Cloudiness: " + clouds + "\n" + "Pressure: " + pressure + "\n" + "Humidity: " + humidity );
-                        mTextViewResult.setText("Weather in: " + poster);
-
+                        mTextViewResult.setText("Movie Titlwe: " + movie_tit);
                     }
                 }, new Response.ErrorListener() {
             @Override
